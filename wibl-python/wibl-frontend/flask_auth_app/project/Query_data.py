6b40a071e4de6d360d0cd81da4213e25bfb7e364
Thomas Ackerly <Thomas.Ackerly@unh.edu>
2024-01-31 20:52:14 -0500

    inital commit with work so far

---
+++ wibl-python/wibl-frontend/flask_auth_app/project/Query_data.py
@@ -0,0 +1,36 @@
+from ast import main
+from os import name
+from flask import Blueprint, render_template, redirect, url_for, request, flash, request, jsonify
+from backend_model import Query
+from __init__ import db2
+
+querydata = Blueprint("querydata", __name__)
+@querydata.route('/query', methods=['GET'])
+def query():
+    try:
+        id1 = request.args.get('id', '')
+        db_instance = db2.query.filter_by(id=id1).first()
+
+        if db_instance:
+         query_result = {"id": db_instance.id, "data": db_instance.data}
+        else:
+            query_result = {"error": "Item not found"}
+
+        return jsonify(query_result)
+    except Exception as e:
+        error_message = {"error": str(e)}
+        return jsonify(error_message), 500
+
+@querydata.route('/submit', methods=['POST'])
+def submit():
+    try:
+        data = request.json.get('data', '')
+        new_db_instance = db2(id="unique_id", data=data)
+        db2.session.add(new_db_instance)
+        db2.session.commit()
+
+        response_data = {"success": "Data submitted successfully"}
+        return jsonify(response_data)
+    except Exception as e:
+        error_message = {"error": str(e)}
+        return jsonify(error_message), 500
---
+++ wibl-python/wibl-frontend/flask_auth_app/project/query_main.py
@@ -0,0 +1,43 @@
+from io import BytesIO
+
+from flask import Flask, render_template, request, send_file
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db2 = SQLAlchemy(app)
+
+class Upload(db2.Model):
+    id = db2.Column(db2.Integer, primary_key=True)
+    filename = db2.Column(db2.String(50))
+    data = db2.Column(db2.LargeBinary)
+
+@app.route('/home', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+
+        upload = Upload(filename=file.filename, data=file.read())
+        db2.session.add(upload)
+        db2.session.commit()
+
+        return f'Uploaded: {file.filename}'
+    elif request.method == 'GET':
+        upload_id = request.args.get('upload_id')
+        if upload_id:
+           upload = Upload.query.filter_by(id=upload_id).first()
+
+           if upload:
+                # Return the file as a downloadable response
+               return send_file(BytesIO(upload.data), attachment_filename=upload.filename, as_attachment=True)
+           else:
+                return "File not found."
+
+        
+    return render_template('home.html')
+
+@app.route('/download/<upload_id>')
+def download(upload_id):
+    upload = Upload.query.filter_by(id=upload_id).first()
+    return send_file(BytesIO(upload.data), attachment_filename=upload.filename, as_attachment=True)
