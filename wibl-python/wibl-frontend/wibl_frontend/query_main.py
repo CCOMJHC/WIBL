993c2c1fc4625a27cc02185addef8952c7a39efd
tim <tnikshych@iol.unh.edu>
2024-04-01 11:11:33 -0400

    WIBL Cleaning up querymain and home files, only takes text input

--- wibl-python/wibl-frontend/wibl_frontend/query_main.py
+++ wibl-python/wibl-frontend/wibl_frontend/query_main.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 import os
-from flask import Flask, render_template, request, send_file, Blueprint
+from flask import Flask, render_template, request, send_file, Blueprint, flash, redirect, url_for
 from flask_cors import CORS, cross_origin
 from fileinput import filename
 from flask_sqlalchemy import SQLAlchemy 
@@ -58,14 +58,14 @@ def home():
     #172.17.0.1 is the "default docker bridge link", required for the local connectivity
     #between containers: https://github.com/HTTP-APIs/hydra-python-agent/issues/104
     # series of GET checks to ensure connectivity
-    connectManager = requests.get('http://172.17.0.1:5000/heartbeat')
+    connectManager = requests.get('http://manager:5000/heartbeat')
     print(f"Result of request to Manager/Heartbeat: {connectManager}")
 
-    connectMoreManager = requests.get('http://172.17.0.1:5000/wibl/all')
+    connectMoreManager = requests.get('http://manager:5000/wibl/all')
     print(f"Result of request to Manager/wibl/all: {connectMoreManager}")
     print(json.dumps(connectMoreManager.json()))
 
-    connectManagerGeoJson = requests.get('http://172.17.0.1:5000/geojson/all')
+    connectManagerGeoJson = requests.get('http://manager:5000/geojson/all')
     print(f"Result of request to Manager/geojson/all: {connectManagerGeoJson}")
     print(json.dumps(connectManagerGeoJson.json()))
     
@@ -79,70 +79,36 @@ def home():
 def index():
 
     print("made it to query_main.index() - file upload")
-    print(UPLOAD_FOLDER)
+    print(UPLOAD_FOLDER) 
+
+    def formatURL():
+        #TODO: make sure input is sanitized!!!
+       f = str(request.get_data().decode())
+       print(f"FILE NAME: {f}")
+       baseURL = 'http://manager:5000/' 
+       url = ''
+       if f.endswith('.wibl'):
+           url = baseURL + 'wibl/' + f
+       elif f.endswith('.geojson'):
+           url = baseURL+ 'geojson/' + f
+       print(f"URL TO GO: {url}") 
+
+       return url
 
     if request.method == 'POST':
 
-        # this method doesnt actually put the wibl file in? rather initializes?
-
-        # check if file is actually part of the post request
-        """
-        if 'file' not in request.files:
-            flash('No file has been selected silly')
-            return redirect(request.url) # verify
-        """
-        f = request.files['file']
-
-        """
-        # check if a file has actually been selected
-        if f.filename == '':
-            flash('No selected file, try again')
-            return redirect(request.url) # verify
-        """
-        fname = secure_filename(f.filename)
-
-        # proof of concept, save to staging 'launchpad' dir
-        #f.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
-        #f.save(fname)
-
-        """
-        with open(UPLOAD_FOLDER + fname, 'r') as thafile:
-            data = json.load(thafile)
-        print(data)
-        """
-        #convertToBinaryData
-        #file = open(str(f.filename), 'rb')
-        
-        payload = MultipartEncoder({'uploadedFile': (fname, f, 'application/octet-stream')})
-        #multipart/form-data
-        #application/octet-stream
-        print("query_main.index() - file name: " + fname)
-        fileNameStripped = os.path.splitext(fname)[0]
-
-        #TODO: Need to check if wibl or geojson
-
-        #print(f"File name without extension: {fileNameStripped}")
-        url = 'http://172.17.0.1:5000/wibl/' + fileNameStripped  #fname
-
-        #headers = {'Content-type': 'application/octet-stream'}
+        print("MADE IT TO POST METHOD")
 
         # post request initializes file, does not need payload or data. doesnt even transfer a file, just initializes space in the DB
-        fileUp = requests.post(url, json={'size':10.4})
+        # distinguish if either wibl/json to correctly set size
+        fileUp = requests.post(formatURL(), json={'size':10.4})
 
         print(f"File Post Status: {fileUp}")
-        """
-        # prototype put request, results in 415 error. File allegedly should be parsed by the manager, and arguments are automatically updated. Or, we could try to parse the file ourselves then manually send the JSON args in the payload
-        fileUp = requests.put(url, json={'logger':'Logger A'})
-        #fileUp = requests.put(url, data=payload, headers={'Content-Type': payload.content_type, 'Accept':payload.content_type}) 
 
-        print(f"File Put Status: {fileUp}")
-        #TODO: modify return statement to redirect back to home.html
+        return redirect(url_for('query_main.index'))
+        #return 'File Uploaded!'
 
-        fileUp = requests.delete(url)
-        print(f"File Delete Status: {fileUp}")
-        """
-        return f'Uploaded: {f.filename}'
-
+    """
     #TODO: needs work
     if request.method == 'GET':
 
@@ -153,12 +119,12 @@ def index():
         print(f"UPLOAD ID: {upload_id}")
         if upload_id:
 
-           url = 'http://172.17.0.1:5000/wibl/' + upload_id
+           url = 'http://manager:5000/wibl/' + upload_id
 
            fileGet = requests.get(url)
            print(json.dumps(fileGet.json()))
            print(f"File Get Status: {fileGet}")
-           """
+
            upload = Upload.query.filter_by(id=upload_id).first()
 
            if upload:
@@ -171,27 +137,20 @@ def index():
 
     if request.method == 'PUT':
         print("IN PUT FUNCTION")
-
-        fileName = request.get_data().decode()
-        print(f"RETURNED STRING: {fileName}")
-
-        url = 'http://172.17.0.1:5000/wibl/' + fileName
-        filePut = requests.put(url, json={'logger':'Logger A', 'platform': 'USS Tallship'})
+        filePut = requests.put(formatURL(), json={'logger':'Logger A', 'platform': 'USS Tallship'})
         print(f"File Update Status: {filePut}")
+        print(json.dumps(filePut.json()))
 
+        #return redirect(url_for('query_main.index'))
         return ''
 
     #Delete Method
     if request.method == "DELETE":
         print("IN DELETE FUNCTION")
-
-        fileName = request.get_data().decode()
-        print(f"RETURNED STRING: {fileName}")
-
-        url = 'http://172.17.0.1:5000/wibl/' + fileName
-        fileDelete = requests.delete(url)
+        fileDelete = requests.delete(formatURL())
         print(f"File Delete Status: {fileDelete}")
 
+        #return redirect(url_for('query_main.index'))
         return ''
 
     return render_template('home.html')
@@ -210,12 +169,17 @@ def download():
 
     print(f"UPLOAD ID: {upload_id}")
 
-    if upload_id:
+    url = 'http://manager:5000/'
 
-       url = 'http://172.17.0.1:5000/wibl/' + upload_id
+    if upload_id.endswith('.wibl'):
+       url = url + 'wibl/' + upload_id
+    elif upload_id.endswith('.geojson'):
+       url = url + 'geojson/' + upload_id
+    else:
+        url += upload_id
 
-       fileGet = requests.get(url)
-       print(json.dumps(fileGet.json()))
-       print(f"File Get Status: {fileGet}")
+    fileGet = requests.get(url)
+    print(json.dumps(fileGet.json()))
+    print(f"File Get Status: {fileGet}")
 
-    return 'File Downloaded'
+    return f'File Downloaded {json.dumps(fileGet.json())}'
