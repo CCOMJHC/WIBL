533afdd250e08292a72957d96427f46254800cea
bmiles_ccom <bmiles@ccom.unh.edu>
2024-10-10 16:18:18 -0400

    frontend: Plumb data from manager to frontend

--- wibl-python/wibl-frontend/src/frontend/tasks.py
+++ wibl-python/wibl-frontend/src/frontend/tasks.py
@@ -1,3 +1,4 @@
+import os
 import time
 import uuid
 
@@ -5,22 +6,42 @@ from celery import shared_task
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
+import requests
 
 @shared_task(name='get-wibl-files')
 def get_wibl_files(session_key: str):
     print("get_wibl_files called!")
     time.sleep(2)
+
+    wibl_files = []
+    wibl_files_data = {'files': wibl_files}
+
     # TODO: Make call to WIBL manager
-    wibl_files_data = {
-        'files': [
-            {
-                'filename': f"{str(uuid.uuid4())}.wibl",
-                'size': 5.4, 'observations': 10232, 'logger': 'UNHJHC-wibl-1',
-                'startTime': '2023-01-23T13:45:08.231', 'status': 1,
-                'messages': 'wibl file test message 1'
-            }
-        ]
-    }
+    manager_url: str = os.environ.get('MANAGEMENT_URL', 'http://manager:5000')
+    wibl_url: str = f"{manager_url}/wibl/all"
+    response = requests.get(wibl_url)
+    if response.status_code != 200:
+        # TODO: Setup logging and log this
+        return False
+    # TODO: Try...except
+    wf_json = response.json()
+    for wf in wf_json:
+        wibl_files.append({
+            "fileid": wf['fileid'],
+            "processtime": wf['processtime'],
+            # TODO: Fill in other fields
+            # "updatetime": "2024-10-10T19:55:53.748479+00:00",
+            # "notifytime": "Unknown",
+            # "logger": "UNHJHC-wibl-1",
+            # "platform": "USCGC Healy",
+            # "size": 1.0,
+            # "observations": 100232,
+            # "soundings": 8023,
+            # "starttime": "2023-01-23T12:34:45.142",
+            # "endtime": "2023-01-24T01:45:23.012",
+            # "status": 0,
+            # "messages": ""
+        })
     channel_layer = get_channel_layer()
     print("sending to websocket...")
     async_to_sync(channel_layer.group_send)(
