8f5453fd4bab9f289bd8e0234baa31e0eb39f100
bmiles_ccom <bmiles@ccom.unh.edu>
2024-10-16 13:05:33 -0400

    frontend: Add CSS styling to web component; Demonstrate interactivity/reactivity with web component

--- wibl-python/wibl-frontend/src/frontend/static/frontend/js/WIBLFileList.js
+++ wibl-python/wibl-frontend/src/frontend/static/frontend/js/WIBLFileList.js
@@ -2,6 +2,14 @@
 // via WebSocket. We will probably want to adapt this into a table view and/or extend
 // an existing Web Component such as smart-table (https://github.com/HTMLElements/smart-table).
 //
+// CSS Stylesheet for this component should be stored in ../css/WIBLFileList.css and passed
+// in via the `styleshee` attribute:
+//
+//      <wibl-file-list id="wibl-file-list1"
+//                      url="{{ wsURL }}"
+//                      stylesheet="{% static 'frontend/css/WIBLFileList.css' %}">
+//      </wibl-file-list>
+//
 // Documentation for creating/editing Web Components can be found on MDN
 // (https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements).
 // and at WHATWG (https://html.spec.whatwg.org/multipage/custom-elements.html).
@@ -11,9 +19,11 @@ class WIBLFileList extends HTMLElement {
         // Always call super first
         super();
         this._url = null;
+        this._styleUrl = null;
+        this._shadow = null;
     }
 
-    static observedAttributes = ["url"];
+    static observedAttributes = ["url", "stylesheet"];
 
     // connectedCallback: Called each time the element is added to the document.
     // The specification recommends that, as far as possible, developers should
@@ -25,10 +35,19 @@ class WIBLFileList extends HTMLElement {
         const sock = SocketManager.getInstance(wsURL, wsType);
 
         // Create shadow DOM root
-        const shadow = this.attachShadow({ mode: "open" });
+        const shadow = this.attachShadow({mode: "open"});
+        this._shadow = shadow;
+
+        // Apply external styles to the shadow dom
+        const linkElem = document.createElement("link");
+        linkElem.setAttribute("rel", "stylesheet");
+        linkElem.setAttribute("href", this._styleUrl);
+        shadow.appendChild(linkElem);
+
         // Create ul to hold list items
         const ul = document.createElement("ul");
         ul.setAttribute("id", "wc-wibl-file-list");
+        ul.className = "class-1"
         shadow.appendChild(ul);
 
         // Create event handler callback and register it with SocketManager
@@ -44,6 +63,7 @@ class WIBLFileList extends HTMLElement {
                 wiblFileList.appendChild(li);
             }
         }
+
         sock.addHandler("list-wibl-files", WIBLFileListHandlerListWiblFiles);
 
         // Request WIBL file data from the WebSocket
@@ -63,20 +83,41 @@ class WIBLFileList extends HTMLElement {
 
     // attributeChangedCallback: Called when attributes are changed, added, removed,
     // or replaced.
+    //
+    // Note: When adding an attribute, make sure to also added it to the `observedAttributes` list above.
+    //
     // See [Responding to attribute changes](https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements#responding_to_attribute_changes)
     // for more details about this callback.
     attributeChangedCallback(name, oldValue, newValue) {
         console.log(`wibl-file-list: Attribute ${name} has changed from ${oldValue} to ${newValue}.`);
-        switch(name) {
+        switch (name) {
             case "url":
                 // URL should only be set on initialization since there is currently
                 // not logic for
                 this._url = newValue;
                 break;
+            case "stylesheet":
+                this._styleUrl = newValue;
+                break;
             default:
                 console.warn(`Unknown attribute ${name}`);
         }
-  }
+    }
+
+    exampleOutsideEventReactivity() {
+        console.log("exampleOutsideEventReactivity() called...");
+        const ul = this._shadow.getElementById("wc-wibl-file-list");
+        switch (ul.className) {
+            case "class-1":
+                ul.className = "class-2";
+                break;
+            case "class-2":
+                ul.className = "class-1";
+                break;
+            default:
+                ul.className = "class-2";
+        }
+    }
 }
 
 // Register custom element
