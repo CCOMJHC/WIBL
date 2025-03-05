90b95c5f48a2ffc172ca59e35d6b91b39943628c
selimnairb <selimnairb@gmail.com>
2025-03-05 15:46:04 -0500

    firmware: Update platformio patch script to use patch.py: Apply all patches idempotently; fail build if patching fails

--- LoggerFirmware/apply_patches.py
+++ LoggerFirmware/apply_patches.py
@@ -1,33 +1,31 @@
 # Override package files as per
 # https://docs.platformio.org/en/latest/scripting/examples/override_package_files.html
-from os.path import join, isfile
+import sys
+from pathlib import Path
 
 Import("env")
 
 PATCHES_PATH = 'platform-patches'
 FRAMEWORK_DIR = env.PioPlatform().get_package_dir("framework-arduinoespressif32")
-patchflag_path = join(FRAMEWORK_DIR, ".patching-done")
 
 print("Patching PlatformIO files...")
 
 patches = {
-    join(PATCHES_PATH, "1-framework-arduinoespressif32-webserver.patch"):
-        join(FRAMEWORK_DIR, "libraries", "WebServer", "src", "WebServer.cpp")
+    Path(PATCHES_PATH, "1-framework-arduinoespressif32-webserver.patch"):
+        Path(FRAMEWORK_DIR, "libraries", "WebServer", "src", "WebServer.cpp")
 }
 
-# patch files only if we didn't do it before
-if not isfile(join(FRAMEWORK_DIR, ".patching-done")):
-    files_were_patched = False
-    for patch_file, original_file in patches.items():
+# Patching with patch.py is idempotent, so always apply patches to make sure all files are patched that ought to be. 
+files_were_patched = False
+for patch_file, original_file in patches.items():
+    print(f"\tApplying patch '{patch_file}' to file '{original_file}'")
+    assert original_file.is_file() and patch_file.is_file()
+    res = env.Execute(f"python patch.py -d {str(original_file.parent)} {str(patch_file)}")
+    if res == 0:
+        print(f"\t\tSuccessfully applied patch '{patch_file}'")
         files_were_patched = True
-        print(f"\tApplying patch '{patch_file}' to file '{original_file}'")
-        assert isfile(original_file) and isfile(patch_file)
-        env.Execute(f"patch {original_file} {patch_file}")
-
-    if files_were_patched:
-        with open(patchflag_path, "w") as fp:
-            fp.write("")
     else:
-        print("\tNo patches to be applied.")
-else:
-    print("\tPatches already applied")
+        sys.exit(f"ERROR: Failed to patch file {original_file}, exiting...")
+
+if not files_were_patched:
+    print("\tNo patches to be applied.")
