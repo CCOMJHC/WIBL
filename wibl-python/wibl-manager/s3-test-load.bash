b65ad6777afd4cc79d241b7f0730271edde389b6
gt1074 <ggtucker04@gmail.com>
2025-03-10 11:10:00 -0400

    OpenLayers Integration

--- wibl-python/wibl-manager/s3-test-load.bash
+++ wibl-python/wibl-manager/s3-test-load.bash
@@ -2,9 +2,5 @@
 
 aws --endpoint-url 'http://localstack:4566' s3api create-bucket --bucket 'wibl-test'
 
-touch test_file.txt
-
-dd if=/dev/urandom of='98C4ED55-190C-40B5-99DF-CC77E1531D1A.wibl' bs=1000 count=10
-
 aws --endpoint-url 'http://localstack:4566' s3api put-object --bucket 'wibl-test' \
-                  --key '98C4ED55-190C-40B5-99DF-CC77E1531D1A.wibl' --body '/opt/code/localstack/98C4ED55-190C-40B5-99DF-CC77E1531D1A.wibl'
+                  --key '047EEFC3-7EEC-4FFD-816F-B6DB15E52297.geojson' --body '/opt/code/localstack/047EEFC3-7EEC-4FFD-816F-B6DB15E52297.geojson'
