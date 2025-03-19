ed6dc431bcad65c1710c186748691260addf46d2
gt1074 <ggtucker04@gmail.com>
2025-03-19 11:51:52 -0400

    Fixed Localstack start up scripting

--- wibl-python/wibl-manager/s3-test-load.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-aws --endpoint-url 'http://localstack:4566' s3api create-bucket --bucket 'wibl-test'
-
-aws --endpoint-url 'http://localstack:4566' s3api put-object --bucket 'wibl-test' \
-                  --key '047EEFC3-7EEC-4FFD-816F-B6DB15E52297.geojson' --body '/opt/code/localstack/047EEFC3-7EEC-4FFD-816F-B6DB15E52297.geojson'
---
+++ wibl-python/wibl-manager/s3-test-load.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+awslocal s3api create-bucket --bucket 'wibl-test'
+
+awslocal s3api put-object --bucket 'wibl-test' \
+                  --key '047EEFC3-7EEC-4FFD-816F-B6DB15E52297.geojson' --body '/opt/code/localstack/047EEFC3-7EEC-4FFD-816F-B6DB15E52297.geojson'
