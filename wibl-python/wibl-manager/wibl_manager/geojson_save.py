4699a920f01def26be6a92ebc242230295c2d8ae
gt1074 <ggtucker04@gmail.com>
2025-03-19 11:29:05 -0400

    Wrapping up initial implementation

--- wibl-python/wibl-manager/wibl_manager/geojson_save.py
+++ wibl-python/wibl-manager/wibl_manager/geojson_save.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
-from flask import Response
+from flask import Response, jsonify
 import boto3
+import json
 import wibl_manager.app_globals as globals
 
 # Creates local client for testing, will eventually be a global configuration
@@ -13,6 +14,10 @@ s3_client = boto3.client('s3',
 # Api gateway with only get functionality
 class GeoJSONSave(Resource):
     def get(self, fileid):
-        # Currently only configured to search a test bucket
-        s3_file = s3_client.get_object(Bucket=globals.S3_BUCKET_NAME, Key=fileid)
-        return Response(s3_file['Body'], content_type="application/geo+json")
+        try:
+            s3_file = s3_client.get_object(Bucket=globals.S3_BUCKET_NAME, Key=fileid)
+            file_content = s3_file['Body'].read().decode('utf-8')
+            json_content = json.loads(file_content)
+            return jsonify(json_content)
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
