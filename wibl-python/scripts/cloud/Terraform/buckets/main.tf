99476eded2f608b12dcdf9aa8e360a91a9244619
gt1074 <ggtucker04@gmail.com>
2025-06-17 16:22:29 -0400

    Making buckets force destroy for testing

--- wibl-python/scripts/cloud/Terraform/buckets/main.tf
+++ wibl-python/scripts/cloud/Terraform/buckets/main.tf
@@ -1,11 +1,14 @@
 resource "aws_s3_bucket" "incoming_bucket" {
     bucket = var.incoming_bucket
+    force_destroy = true
 }
 
 resource "aws_s3_bucket" "staging_bucket" {
     bucket = var.staging_bucket
+    force_destroy = true
 }
 
 resource "aws_s3_bucket" "viz_bucket" {
     bucket = var.viz_bucket
+    force_destroy = true
 }
