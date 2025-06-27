33b2cfeeaca2bbfc606589f66dec379aab8de8cc
gt1074 <ggtucker04@gmail.com>
2025-06-27 15:54:34 -0400

    Started wibl_data transition

--- wibl-python/wibl-manager/src/wibl_manager/database.py
+++ wibl-python/wibl-manager/src/wibl_manager/database.py
@@ -1,9 +1,22 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker, scoped_session, Session
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from fastapi import Depends
 import os
+
 Base = declarative_base()
 
+MANAGER_DATABASE_URI = os.environ.get('MANAGER_DATABASE_URI', "postgresql://postgres:postgres@db:5432/postgres")
+
+engine = create_async_engine(MANAGER_DATABASE_URI)
+SessionLocal = async_sessionmaker(expire_on_commit=False, class_=AsyncSession, bind=engine)
 
 def get_db_url():
     return f"postgresql://{os.environ['DATABASE_USER']}:{os.environ['DATABASE_PASSWORD']}@" \
                f"{os.environ['DATABASE_HOST']}:{os.environ['DATABASE_PORT']}/" \
                f"{os.environ['DATABASE_NAME']}"
+
+
+async def get_async_db():
+    with SessionLocal() as session:
+        yield session
+
