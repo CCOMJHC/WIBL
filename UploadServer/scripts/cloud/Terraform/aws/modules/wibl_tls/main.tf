a6b90134158126ded252d9098859c63135e9f545
selimnairb <selimnairb@gmail.com>
2025-10-20 15:57:18 -0400

    upload-server: Factor variables and outputs from wibl-tls terraform module

--- UploadServer/scripts/cloud/Terraform/aws/modules/wibl_tls/main.tf
+++ UploadServer/scripts/cloud/Terraform/aws/modules/wibl_tls/main.tf
@@ -11,67 +11,6 @@ terraform {
   }
 }
 
-# Variables for certificate subjects
-variable "out_dir" {
-  description = "Output directory for certificates"
-  type        = string
-  default     = "certs"
-}
-
-variable "ca_common_name" {
-  description = "Common name for CA certificate"
-  type        = string
-  default     = "localhost"
-}
-
-variable "server_common_name" {
-  description = "Common name for server certificate"
-  type        = string
-  default     = "localhost"
-}
-
-variable "client_hostname" {
-  description = "Hostname for client certificate"
-  type        = string
-  default     = "wibl-logger"
-}
-
-variable "server_cert_dns_names" {
-  description = "DNS names for server certificate"
-  type        = list(string)
-  default     = ["localhost"]
-}
-
-variable "server_cert_ip_addrs" {
-  description = "IP addresses for server certificate"
-  type        = list(string)
-  default     = ["127.0.0.1"]
-}
-
-variable "country" {
-  description = "Country code for certificate subjects"
-  type        = string
-  default     = "US"
-}
-
-variable "state" {
-  description = "State for certificate subjects"
-  type        = string
-  default     = "NewHampshire"
-}
-
-variable "locality" {
-  description = "Locality for certificate subjects"
-  type        = string
-  default     = "Durham"
-}
-
-variable "organization" {
-  description = "Organization for certificate subjects"
-  type        = string
-  default     = "CCOM-JHC"
-}
-
 # CA Certificate (Self-signed)
 resource "tls_private_key" "ca" {
   algorithm = "RSA"
@@ -169,10 +108,4 @@ resource "local_file" "server_crt" {
   content         = tls_locally_signed_cert.server.cert_pem
   filename        = "${var.out_dir}/server.crt"
   file_permission = "0644"
-}
-
-# Outputs for easy access to certificate details
-output "certificate_directory" {
-  description = "Directory where certificates were written"
-  value       = var.out_dir
 }
